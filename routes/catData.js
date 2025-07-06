56a3715a3ace46c11b598208033f224fa0347d33
IvinGeorge04 <ivingeorge04@gmail.com>
2025-07-06 12:08:32 +0530

    made by ivin

--- routes/catData.js
+++ routes/catData.js
@@ -1,16 +1,16 @@
 import express from "express";
-import catDataModel from "../model/catDataModel.js";
+import CatDataModel from "../model/CatDataModel.js";
 
 const router = express.Router();
 
 router.get("/",async (req, res) => {
-    const holders = await catDataModel.find().sort({rank:1});
+    const holders = await CatDataModel.find().sort({rank:1});
     res.json(holders);
 });
 
 router.post("/",async(req,res) => {
     const { name,gender,city,image,discription,num,age } = req.body;
-    const newHolder = new catDataModel({ name,gender,city,image,discription,num,age });
+    const newHolder = new CatDataModel({ name,gender,city,image,discription,num,age });
     await newHolder.save();
     res.status(201).json(newHolder);
 })
